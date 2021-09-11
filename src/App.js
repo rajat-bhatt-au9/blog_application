403b726877f3cd05a2ffae6b1521de484992a60c
Rajat Bhatt <rajatbhatt500@gmail.com>
2021-09-11 11:39:56 +0530

    added ant design

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+//import "./App.css";
+import { DatePicker } from "antd";
+import "antd/dist/antd.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +19,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+
+      <DatePicker />
     </div>
   );
 }
